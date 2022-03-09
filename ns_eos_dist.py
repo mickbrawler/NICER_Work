ca6f36bb16038ea5cbaa7fff9427d65b0fa54adb
Michael Camilo <camilom1@montclair.edu>
2022-03-09 15:46:46 -0500

    New fractional error plots.

--- ns_eos_dist.py
+++ ns_eos_dist.py
@@ -8,16 +8,17 @@ import scipy.stats as st
 
 # This file's functions will produce the proper distribution for the NICER project
 
-def eos_radii_lambdas(eos_name, N, m_sigma, r_sigma, name="test"):
-    # Function that produces the possible masses, radii, lambdas for any equation of state
+def eos_radii_posterior(eos_name, N, name="test"):
+    # Function that produces the possible masses and radii for any equation of state
+
+    m_sigma = .4
+    r_sigma = 500
 
     eos = lalsim.SimNeutronStarEOSByName(eos_name)
     fam = lalsim.CreateSimNeutronStarFamily(eos)
 
     working_masses = []
     working_radii = []
-    working_Lambdas = []
-    Lambdas = []
     N_count = 0
     while N_count < N:
         try:
@@ -28,19 +29,18 @@ def eos_radii_lambdas(eos_name, N, m_sigma, r_sigma, name="test"):
             kk = lalsim.SimNeutronStarLoveNumberK2(m*lal.MSUN_SI, fam)
             cc = m*lal.MRSUN_SI/rr
             Lambdas = (2/3)*kk/(cc**5)
-            working_Lambdas.append(Lambdas)
             working_masses.append(m)
             working_radii.append(rr)
             N_count += 1
         except RuntimeError:
             continue
 
-    output = np.vstack((working_masses,working_radii,working_Lambdas)).T
-    outputfile = "NICER_mock_data/mean_radii_lambdas/{}.txt".format(name)
-    np.savetxt(outputfile, output, fmt="%f\t%f\t%f")
+    output = np.vstack((working_masses,working_radii)).T
+    outputfile = "NICER_mock_data/mean_radii_lambdas/mass_radii_{}.txt".format(name)
+    np.savetxt(outputfile, output, fmt="%f\t%f")
 
-def plot_radii(datafile, name):
-    # Function to plot eos' radii
+def plot_radii_scatter(datafile, name):
+    # Function to plot scatter of eos' radii
 
         pl.clf()
         data = np.loadtxt(datafile)
@@ -49,30 +49,13 @@ def plot_radii(datafile, name):
 
         pl.rcParams.update({"font.size":18})
         pl.figure(figsize=(20,10))
-        pl.scatter(masses,radii)
+        pl.scatter(masses,radii,s=5)
         pl.xlabel("Mass")
         pl.ylabel("Radius")
         pl.title("Radii vs Masses")
-        pl.savefig("NICER_mock_data/radii_plots/{}.png".format(name))
+        pl.savefig("NICER_mock_data/radii_plots/mass_radii_{}.png".format(name))
 
-def plot_radii_heat(datafile, bins, name, save=True):
-    # Function to plot eos' radii as heatmap
-
-    pl.clf()
-    data = np.loadtxt(datafile)
-    masses = data[:,0]
-    radii = data[:,1]
-
-    heatmap, xedges, yedges = np.histogram2d(masses, radii, bins=bins) # creates a 2d grid with the intensities, and the coordinate values for each bin
-    
-    pl.imshow(heatmap.T, origin='lower') # Apparently imshow transposes the image and plots the origin in the upper left by default so this is accounted for
-    pl.xlabel("Mass")
-    pl.ylabel("Radius")
-    pl.colorbar()
-
-    if save: pl.savefig("NICER_mock_data/radii_heat_plots/{}.png".format(name))
-
-def radii_gaussian_kde_plot(datafile, name, save=True):
+def plot_radii_gaussian_kde(datafile, name, save=True):
     # Plot the kde of the eos' radii distribution
 
     pl.clf()
@@ -95,18 +78,36 @@ def radii_gaussian_kde_plot(datafile, name, save=True):
     ax.set_xlim(m_min, m_max)
     ax.set_ylim(r_min, r_max)
 
-    cfset = ax.pcolormesh(mm, rr, f, cmap='Blues')
+    ax.pcolormesh(mm, rr, f)
     ax.set_xlabel('Mass')
     ax.set_ylabel('Radius')
     pl.scatter(m,r,s=5,color="black")
 
-    if save: pl.savefig("NICER_mock_data/radii_heat_plots/{}.png".format(name))
+    if save: pl.savefig("NICER_mock_data/radii_heat_plots/mass_radii_{}.png".format(name))
 
-def plot_radii_kde_heat(datafile, bins, name, save=True):
+def plot_radii_heat(datafile, bins, name, save=True):
+    # Function to plot eos' radii as heatmap
+
+    pl.clf()
+    data = np.loadtxt(datafile)
+    masses = data[:,0]
+    radii = data[:,1]
+
+    heatmap, xedges, yedges = np.histogram2d(masses, radii, bins=bins) # creates a 2d grid with the intensities, and the coordinate values for each bin
+    
+    pl.imshow(heatmap.T, origin='lower') # Apparently imshow transposes the image and plots the origin in the upper left by default so this is accounted for
+    pl.xlabel("Mass")
+    pl.ylabel("Radius")
+    pl.colorbar()
+
+    if save: pl.savefig("NICER_mock_data/radii_heat_plots/mass_radii_{}.png".format(name))
+
+def plot_radii_kde_heat(datafile, bins, name, save=True, N=1000):
     # Plot the heat and kde of the eos' radii distribution
 
     pl.clf()
-    fig, (ax1, ax2) = pl.subplots(1, 2)
+    fig, (ax1, ax2) = pl.subplots(1, 2, figsize=(20,15))
+    pl.rcParams["font.size"] = "16"
 
     data = np.loadtxt(datafile)
     masses = data[:,0]
@@ -122,56 +123,11 @@ def plot_radii_kde_heat(datafile, bins, name, save=True):
     kernel = st.gaussian_kde(pairs)
     f = np.reshape(kernel(positions).T, mm.shape)
 
-    ax1 = fig.gca()
     ax1.set_xlim(m_min, m_max)
     ax1.set_ylim(r_min, r_max)
 
-    cfset = ax1.pcolormesh(mm, rr, f, cmap='Blues')
-    ax1.set_xlabel('Mass')
-    ax1.set_ylabel('Radius')
+    cfset = ax1.pcolormesh(mm, rr, f)
     ax1.scatter(masses,radii,s=5,color="black")
-
-    heatmap, xedges, yedges = np.histogram2d(masses, radii, bins=bins) # creates a 2d grid with the intensities, and the coordinate values for each bin
-    
-    ax2.imshow(heatmap.T, origin='lower') # Apparently imshow transposes the image and plots the origin in the upper left by default so this is accounted for
-    ax2.set_xlabel("Mass")
-    ax2.set_ylabel("Radius")
-
-    if save: pl.savefig("NICER_mock_data/radii_heat_plots/{}.png".format(name))
-
-def plot_lambdas(datafile, name):
-    # Function to plot eos' lambdas
-
-        pl.clf()
-        data = np.loadtxt(datafile)
-        masses = data[:,0]
-        lambdas = data[:,2]
-
-        pl.rcParams.update({"font.size":18})
-        pl.figure(figsize=(20,10))
-        pl.scatter(masses,lambdas)
-        pl.xlabel("Mass")
-        pl.ylabel("Lambdas")
-        pl.title("Lambdas vs Masses")
-        pl.savefig("NICER_mock_data/lambdas_plots/{}.png".format(name))
-
-def plot_multiple_lambdas(datafile, name, N):
-    # Function to plot eos' mean produced lambdas and multiple separate eos' lambda curves
-    
-    pl.clf()
-    data = np.loadtxt(datafile)
-    masses = data[:,0]
-    Lambdas = data[:,2]
-    pl.rcParams.update({"font.size":18})
-    pl.figure(figsize=(20,10))
-    pl.scatter(masses,Lambdas,color="brown")
-
-    all_eos = lalsim.SimNeutronStarEOSNames
-    GWX_list = ["BHF_BBB2","KDE0V","KDE0V1","SKOP","HQC18","SLY2",
-                "SLY230A","SKMP","RS","SK255","SLY9","APR4_EPP",
-                "SKI2","SKI4","SKI6","SK272","SKI3","SKI5","MPA1",
-                "MS1B_PP","MS1_PP","BBB2","AP4","MPA1","MS1B","MS1",
-                "SLY"]
 
     short_eos_list = ["APR4_EPP","SKOP","MPA1"]
     for eos_name in short_eos_list:
@@ -180,24 +136,122 @@ def plot_multiple_lambdas(datafile, name, N):
         m_min = 1.0
         max_mass = lalsim.SimNeutronStarMaximumMass(fam)/lal.MSUN_SI
         max_mass = int(max_mass*1000)/1000
-        masses = np.linspace(m_min, max_mass, N)
-        masses = masses[masses <= max_mass]
+        mm = np.linspace(m_min, max_mass, N)
+        mm = mm[mm <= max_mass]
 
         working_masses = []
-        working_Lambdas = []
-        Lambdas = []
-        for m in masses:
+        working_radii = []
+        for m in mm:
             try:
-                radius = lalsim.SimNeutronStarRadius(m*lal.MSUN_SI, fam)
                 rr = lalsim.SimNeutronStarRadius(m*lal.MSUN_SI, fam)
-                kk = lalsim.SimNeutronStarLoveNumberK2(m*lal.MSUN_SI, fam)
-                cc = m*lal.MRSUN_SI/rr
-                Lambdas = (2/3)*kk/(cc**5)
-                working_Lambdas.append(Lambdas)
                 working_masses.append(m)
+                working_radii.append(rr)
             except RuntimeError:
                 break
-        pl.plot(masses,working_Lambdas,label=eos_name)
-    pl.title("Lambdas vs Masses")
-    pl.legend()
-    pl.savefig("NICER_mock_data/multiple_lambdas_plots/{}.png".format(name))
+        ax1.plot(working_masses,working_radii,label=eos_name)
+    ax1.legend()
+
+    heatmap, xedges, yedges = np.histogram2d(masses, radii, bins=bins) # creates a 2d grid with the intensities, and the coordinate values for each bin
+    
+    ax2.imshow(heatmap.T, origin="lower") # Apparently imshow transposes the image and plots the origin in the upper left by default so this is accounted for
+
+    pl.suptitle("APR4_EPP: Mass & Radius Distribution")
+    fig.text(0.5, 0.01, "Mass", ha="center")
+    fig.text(0.01, 0.5, "Radius", va="center", rotation="vertical")
+
+    if save: pl.savefig("NICER_mock_data/radii_heat_plots/mass_radii_{}.png".format(name))
+
+def density_along_curve(datafile, N=1000):
+    # Finds integral of eos curves over the kde of a mass-radius posterior sample
+
+    masses, radii = np.loadtxt(datafile, unpack=True)
+    pairs = np.vstack([masses, radii])
+    kernel = st.gaussian_kde(pairs)
+
+    short_eos_list = ["APR4_EPP","SKOP","MPA1"]
+    sum_list = []
+    for eos_name in short_eos_list:
+        eos = lalsim.SimNeutronStarEOSByName(eos_name)
+        fam = lalsim.CreateSimNeutronStarFamily(eos)
+        m_min = np.min(masses)
+        max_mass = lalsim.SimNeutronStarMaximumMass(fam)/lal.MSUN_SI
+        max_mass = int(max_mass*1000)/1000
+        m_grid = np.linspace(m_min, max_mass, N)
+        m_grid = m_grid[m_grid <= max_mass]
+
+        working_masses = []
+        working_radii = []
+        for m in m_grid:
+            try:
+                rr = lalsim.SimNeutronStarRadius(m*lal.MSUN_SI, fam)
+                working_masses.append(m)
+                working_radii.append(rr)
+            except RuntimeError:
+                break
+        sum_list.append(np.sum(kernel(np.vstack([working_masses, working_radii]))*np.diff(working_masses)[0]))
+    return(sum_list)
+
+def likelihood(datafile, eos_name, N=1000):
+    # Finds integral of eos curve over the kde of a mass-radius posterior sample
+
+    # Bottom 3 lines should be separated into init() of a class in the future
+    masses, radii = np.loadtxt(datafile, unpack=True)
+    pairs = np.vstack([masses, radii])
+    kernel = st.gaussian_kde(pairs)
+
+    eos = lalsim.SimNeutronStarEOSByName(eos_name)
+    fam = lalsim.CreateSimNeutronStarFamily(eos)
+    m_min = np.min(masses)
+    max_mass = lalsim.SimNeutronStarMaximumMass(fam)/lal.MSUN_SI
+    max_mass = int(max_mass*1000)/1000
+    m_grid = np.linspace(m_min, max_mass, N)
+    m_grid = m_grid[m_grid <= max_mass]
+
+    working_masses = []
+    working_radii = []
+    for m in m_grid:
+        try:
+            rr = lalsim.SimNeutronStarRadius(m*lal.MSUN_SI, fam)
+            working_masses.append(m)
+            working_radii.append(rr)
+        except RuntimeError:
+            break
+    integral = np.sum(kernel(np.vstack([working_masses, working_radii]))*np.diff(working_masses)[0])
+    return(integral)
+
+def error_barplot(datafile, name): 
+
+    eos_list = ["BHF_BBB2","KDE0V","SKOP","RS","APR4_EPP","SKI6","MPA1","AP4"]
+    Ns = [100,1000,100000]
+
+    N1_integral = []
+    N2_integral = []
+    true_integral = []
+    for eos_name in eos_list:
+        for N in Ns:
+            integral = likelihood(datafile, eos_name, N=N)
+            if Ns.index(N)==0: N1_integral.append(integral)
+            elif Ns.index(N)==1: N2_integral.append(integral)
+            else: true_integral.append(integral)
+    
+    N1_integral = np.array(N1_integral)
+    N2_integral = np.array(N2_integral)
+    true_integral = np.array(true_integral)
+    N1 = abs(true_integral - N1_integral) / true_integral
+    N2 = abs(true_integral - N2_integral) / true_integral
+
+    x = np.arange(len(eos_list))  # the label locations
+    width = 0.35  # the width of the bars
+
+    fig, ax = pl.subplots()
+    ax.bar(x - width/2, N1, width, label="N_100")
+    ax.bar(x + width/2, N2, width, label="N_1000")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Error')
+    ax.set_title('EOS Curve Sizes')
+    ax.set_xticks(x)
+    ax.set_xticklabels(eos_list)
+    ax.legend()
+    pl.savefig("NICER_mock_data/error_plots/mass_radii_{}.png".format(name))
+
