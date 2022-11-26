69329136040f1bfdc9291d072236d90afd0b18d7
Michael Camilo <camilom1@montclair.edu>
2022-11-26 18:18:37 -0500

    Organized directories before big dev runs!

--- joint_inference/development/multiple_confidence_interval.py
+++ joint_inference/development/multiple_confidence_interval.py
@@ -8,9 +8,12 @@ def plot_intervals(outputfile):
 
     pl.clf()
     
-    Files = ["./borrowed_data/spectral_p_vs_rho_W10_S100000.txt", "./new_data/spectral_p_vs_rho_thinned_GW_spectral_confidence.txt", "new_data/spectral_p_vs_rho_GW_EM_spectral_sampled_confidence_viacs.txt"]
-    labels = ["EM confidence method", "GW confidence method", "merger confidence method"]
-    colors = ["blue", "red", "green"]
+    #Files = ["./borrowed_data/spectral_p_vs_rho_W10_S100000.txt", "./new_data/spectral_p_vs_rho_thinned_GW_spectral_confidence.txt", "new_data/spectral_p_vs_rho_GW_EM_spectral_sampled_confidence_viacs.txt"]
+    Files = ["./new_data/spectral_p_vs_rho_thinned_GW_spectral_confidence.txt", "new_data/spectral_p_vs_rho_GW_EM_spectral_sampled_confidence_viacs.txt"]
+    #labels = ["EM confidence method", "GW confidence method", "merger confidence method"]
+    labels = ["GW", "EM + GW"]
+    colors = ["red", "green"]
+    #labels = ["EM confidence method", "GW confidence method", "merger confidence method"]
 
     for File, label, color in zip(Files,labels,colors):
 
