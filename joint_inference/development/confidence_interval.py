ede43f2e07971fc70a84ec3228ca0ff7d895451e
Michael Camilo <camilom1@montclair.edu>
2023-03-26 00:24:08 -0400

    Plots of plots resembling Anarya's. 20th attempt.

--- joint_inference/development/confidence_interval.py
+++ joint_inference/development/confidence_interval.py
@@ -6,9 +6,9 @@ import corner
 def plot_intervals(outputfile):
     # Plots differently sourced confidence intervals on top of each other.
 
-#    Files = ["run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_GW170817_confidence.txt"]
-#    labels = ["GW170817"]
-#    colors = ["#d95f02"]
+    Files = ["run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_GW170817_confidence.txt"]
+    labels = ["GW170817"]
+    colors = ["#d95f02"]
 
 #    Files = ["run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_J0740_confidence.txt"]
 #    labels = ["J0740"]
@@ -18,12 +18,12 @@ def plot_intervals(outputfile):
 #    labels = ["GW+EM"]
 #    colors = ["#1b9e77"]
 
-    Files = ["run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_GW170817_confidence.txt", "run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_J0740_confidence.txt", "run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_GW170817_J0740_hierarchical_confidence.txt"]
-    labels = ["GW170817", "J0740", "GW+EM"]
-    colors = ["#d95f02", "#7570b3", "#000000"]
+#    Files = ["run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_GW170817_confidence.txt", "run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_J0740_confidence.txt", "run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_GW170817_J0740_hierarchical_confidence.txt"]
+#    labels = ["GW170817", "J0740", "GW+EM"]
+#    colors = ["#d95f02", "#7570b3", "#000000"]
 
     pl.clf()
-    pl.figure(figsize=(12,12))
+    pl.figure(figsize=(15,12))
     pl.rc('font', size=20)
     pl.rc('axes', facecolor='#E6E6E6', edgecolor='black')
     pl.rc('xtick', direction='out', color='black', labelcolor='black')
