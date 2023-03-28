0bacffe1900fbd44783d85c27c93512677952889
Michael Camilo <camilom1@montclair.edu>
2023-03-27 21:42:33 -0400

    Plots. 34 attempt.

---
+++ joint_inference/development/SpringPresentation.py
@@ -0,0 +1,49 @@
+from GWXtreme import eos_prior as ep
+from multiprocessing import cpu_count, Pool
+from scipy import interpolate
+import scipy.stats as st
+import lalsimulation as lalsim
+import lal
+import numpy as np
+import matplotlib.pyplot as pl
+import seaborn as sns
+import h5py
+import json
+import emcee
+import math
+import random
+import argparse
+
+def namedEoS_p_rho(EoS_Name):
+    
+    N = 1000
+    min_log_pressure = 31.7
+    max_log_pressure = 35.0
+
+    logp_grid = np.linspace(min_log_pressure, max_log_pressure, N+1)
+    logp_grid = logp_grid[:-1] # last val is max log pressure. For spectral method, density computation at this pressure causes a runtime error
+
+    density_grid = []
+    for lp in logp_grid:
+
+        eos = lalsim.SimNeutronStarEOSByName(EoS_Name)
+        try:
+            density_grid.append(lalsim.SimNeutronStarEOSEnergyDensityOfPressure(10**lp, eos)/lal.C_SI**2)
+        except RuntimeError: 
+            continue # ran into runtime error at some point due to energydensityofpressure function
+
+    pl.figure(figsize=(12,12))
+    pl.rc('font', size=20)
+    pl.rc('axes', facecolor='#E6E6E6', edgecolor='black')
+    pl.rc('xtick', direction='out', color='black', labelcolor='black')
+    pl.rc('ytick', direction='out', color='black', labelcolor='black')
+    pl.rc('lines', linewidth=2)
+
+    pl.plot(np.log(density_grid), np.log(logp_grid), label=EoS_Name)
+
+    pl.xlim([17.1, 18.25])
+    pl.xlabel(r'$log10(\frac{kg}{m^3})$')
+    pl.ylabel(r'$log10(Pa)$')
+    pl.legend()
+    pl.savefig("plots/"+EoS_Name+"_p_rho.png", bbox_inches='tight')
+
