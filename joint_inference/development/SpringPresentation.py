caca4385d220070d205d26a41ee14194464e744c
Michael Camilo <camilom1@montclair.edu>
2023-03-27 22:24:49 -0400

    Plots. 38 attempt.

--- joint_inference/development/SpringPresentation.py
+++ joint_inference/development/SpringPresentation.py
@@ -47,3 +47,50 @@ def namedEoS_p_rho(EoS_Name):
     pl.legend()
     pl.savefig("plots/"+EoS_Name+"_p_rho.png", bbox_inches='tight')
 
+def overlap_namedEoS_constraint_p_rho(EoS_Names):
+
+    N = 1000
+    min_log_pressure = 31.7
+    max_log_pressure = 35.0
+
+    logp_grid = np.linspace(min_log_pressure, max_log_pressure, N+1)
+    logp_grid = logp_grid[:-1] # last val is max log pressure. For spectral method, density computation at this pressure causes a runtime error
+
+    pl.figure(figsize=(12,12))
+    pl.rc('font', size=20)
+    pl.rc('axes', facecolor='#E6E6E6', edgecolor='black')
+    pl.rc('xtick', direction='out', color='black', labelcolor='black')
+    pl.rc('ytick', direction='out', color='black', labelcolor='black')
+    pl.rc('lines', linewidth=2)
+
+    File = "run_data/8th_cutoff_plotting/spectral_p_vs_rho_cutoff_W100_S10000_GW170817_J0030_hierarchical_confidence.txt"
+    label = "GW+EM"
+    color = "#000000"
+    logp_grid, lower_bound, median, upper_bound = np.loadtxt(File).T
+    lower_bound, upper_bound = np.log10(lower_bound), np.log10(upper_bound)
+    ax1 = pl.gca()
+    pl.plot(lower_bound, logp_grid, label=label, color=color)
+    pl.plot(upper_bound, logp_grid, color=color)
+    ax1.fill_betweenx(logp_grid, lower_bound, x2=upper_bound, color=color, alpha=0.45)
+
+    for EoS_Name in EoS_Names:
+
+        density_grid = []
+        safety_logp_grid = []
+        for lp in logp_grid:
+
+            eos = lalsim.SimNeutronStarEOSByName(EoS_Name)
+            try:
+                density_grid.append(lalsim.SimNeutronStarEOSEnergyDensityOfPressure(10**lp, eos)/lal.C_SI**2)
+                safety_logp_grid.append(lp)
+            except RuntimeError: 
+                continue # ran into runtime error at some point due to energydensityofpressure function
+
+        pl.plot(np.log10(density_grid), safety_logp_grid, label=EoS_Name)
+
+    pl.xlim([17.1, 18.25])
+    pl.xlabel(r'$log10(\frac{kg}{m^3})$')
+    pl.ylabel(r'$log10(Pa)$')
+    pl.legend()
+    pl.savefig("plots/overlap_namedEoS_constraint_p_rho.png", bbox_inches='tight')
+
