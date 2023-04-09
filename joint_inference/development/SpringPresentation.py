aed999c2c15c9efdb20e03aa3ce75d7158d44769
Michael Camilo <camilom1@montclair.edu>
2023-04-09 02:53:11 -0400

    Final CASABONA plot revisions 12

--- joint_inference/development/SpringPresentation.py
+++ joint_inference/development/SpringPresentation.py
@@ -69,7 +69,7 @@ def overlap_namedEoS_constraint_p_rho(EoS_Names):
     pl.plot(upper_bound, logp_grid, color=color)
     ax1.fill_betweenx(logp_grid, lower_bound, x2=upper_bound, color=color, alpha=0.45)
 
-    N = 1000
+    #N = 1000
     #min_log_pressure = 31.5 # Adjusted from 31.7 for overlap plot
     #max_log_pressure = 35.0
     #logp_grid = np.linspace(min_log_pressure, max_log_pressure, N+1)
@@ -87,7 +87,7 @@ def overlap_namedEoS_constraint_p_rho(EoS_Names):
             except RuntimeError: 
                 continue # ran into runtime error at some point due to energydensityofpressure function
 
-        pl.plot(density_grid, safety_logp_grid, label=EoS_Name)
+        pl.plot(density_grid, safety_logp_grid, label=EoS_Name, color="black")
 
     pl.vlines(x=2.3*10**17,ymin=min(logp_grid),ymax=max(logp_grid),color="red")
     pl.text(10**17.75,10**34,"Super-Nuclear Density",fontsize=20)
